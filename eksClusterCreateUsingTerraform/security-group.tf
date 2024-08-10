90c5e4436603f46e3c771ba4ddf1ef74f36f701c
sureshpasam <sureshpasam94@gmail.com>
2024-08-10 12:16:43 +0530

    eksClustercreateTerraform

---
+++ eksClusterCreateUsingTerraform/security-group.tf
@@ -0,0 +1,13 @@
+
+}
+
+resource "aws_security_group_rule" "all_worker_mgmt_egress" {
+  description       = "allow outbound traffic to anywhere"
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
